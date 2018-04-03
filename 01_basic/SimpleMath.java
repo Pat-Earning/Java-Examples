b21a16bee982273babff76bcd551bcff6867466d
Pat-Earning <33377292+Pat-Earning@users.noreply.github.com>
2018-04-03 12:55:04 +0200

    Create SimpleMath.java

---
+++ 01_basic/SimpleMath.java
@@ -0,0 +1,13 @@
+package javaexamples.basic;
+
+public class SimpleMath {
+    
+    public static void main() {
+               
+    }
+    
+    public static void Factorial(int num){
+       int fact = 1;
+       for (int i=1; i<= num
+    }
+}
